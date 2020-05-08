43e73ab6ce4d3c7687e1a9ee3f9ebf7abee53d9c
Rjj1512 <jhaveri.rahil@gmail.com>
2020-05-08 20:43:45 +0530

    Filter Search and Sorting Done for Hospitals

--- src/components/Hospital.js
+++ src/components/Hospital.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
-import './Admin.css'
+import ReactDOM from "react-dom";
+import './Admin.css';
+import './List.css';
 
 class Hospital extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {show: false, filter: ''}
+  }
+
+  showModal = () => {
+    console.log("here")
+    this.setState({ show: true });
+  };
+
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  changeFilter = (type) => {
+    this.setState({ filter: type})
+  }
+  
   render() {
     return (
       <div id="content" style={{ justifyContent: 'center', width: '100%' }}>
@@ -70,6 +91,13 @@ class Hospital extends Component {
           </div>
         }}> */}
         <h4 style={{ justifyContent: 'center' }}>Available blood bags</h4>
+        <List prs = {this.props} show={this.state.show}
+         handleClose={this.hideModal} 
+         filter={this.state.filter} changeFilter={this.changeFilter}>
+        </List>
+        <button className="btn btn-primary float-right" style={{backgroundColor: 'green', display: 'flex', justifyContent: 'right'}} type="button" onClick={this.showModal}>
+          Search Blood Bags
+        </button>
           <table className="table">
             <thead>
               <tr>
@@ -83,10 +111,11 @@ class Hospital extends Component {
             </thead>
             <tbody id="Options list">
               {this.props.bags.sort((a, b) => b.expiry - a.expiry).reverse().slice(0, 10).map((bag, key) => {
-                if (bag.owner !== this.props.account && bag.used !== true /*&& bag.blood_group === "AB-"*/) {
+                if (bag.owner !== this.props.account && bag.used !== true) {
                   let a = bag.id.toNumber()
                   console.log(a)
                   console.log(bag.expiry,"expiry")
+                  const expiry = (new Date(bag.expiry * 1000)).toString()
                   return (
                     <tr key={key}>
                       <th scope="row">{bag.id.toString()}</th>
@@ -112,5 +141,88 @@ class Hospital extends Component {
     );
   }
 }
+
+const List = ({ handleClose, show, prs, filter, changeFilter}) => {
+  const showHideClassname = show ? "modal display-block" : "modal display-none";
+  var i = 0
+  return (
+      <div className={showHideClassname}>
+      <section className="modal-main">
+      <p>&nbsp;</p>
+      <div>
+      <form class='form-inline' onSubmit={(event) => {
+          event.preventDefault()
+          const type = event.target[0].value
+          changeFilter(type)
+        }}>
+          <div className="form-group mr-sm-2">
+            <input 
+              id="bloodtype"
+              type="text"
+              className="form-control"
+              placeholder="Blood Group" />
+          </div>        
+          <button type="submit" className="btn btn-primary">Search</button>
+        </form>
+        </div>
+        <br/>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Collector Bank's (Name and Address)</th>
+            <th scope="col">Current Owner's Name and Address</th>
+            <th scope="col">Blood Group</th>
+            <th scope="col">Expiry</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody id="Bags list">
+          {prs.bags.sort((a, b) => b.expiry - a.expiry).reverse().map((bag, key) => {
+            if (bag.owner !== prs.account && bag.used !== true) {
+              /*console.log(bag.user)
+              if (bag.user_type != 1){
+                return true
+              }*/
+              if (filter !=''){
+                if (bag.blood_group != filter) {
+                  return true
+                }
+              }
+              console.log(filter,bag.blood_group)
+                i = i+1
+              console.log(bag.expiry,"expiry")
+              const expiry = (new Date(bag.expiry * 1000)).toString()
+              return(
+                <tr key={key}>
+                <th scope="row">{i}</th>
+                <td>{prs.usertype[bag.bank].name} <br /> {bag.bank} </td>
+                <td>{prs.usertype[bag.owner].name} <br /> {bag.owner} </td>
+                <td>{bag.blood_group}</td>
+                <td>{expiry.slice(0,15)}</td>
+                <th scope="row">
+                  <button
+                    name={bag.id.toNumber()}
+                    className="btn btn-primary"
+                    onClick={(event) => {
+                      prs.h_placeOrder(event.target.name)
+                    }}>Buy for 1.5 ETH </button></th>
+                </tr>
+              )
+            }
+          })}
+        </tbody>
+      </table>
+          <button className="btn btn-primary" 
+          style={{margin:'auto', display: 'block',backgroundColor: 'red', display: 'flex', justifyContent: 'center'}}
+           onClick={handleClose}>close</button>
+          <br/>
+      </section>
+      </div>
+  );
+};
+
+const container = document.createElement("div");
+document.body.appendChild(container);
 
 export default Hospital;
